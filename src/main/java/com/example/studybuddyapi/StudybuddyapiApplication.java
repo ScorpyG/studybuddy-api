dcc86232e803fc039bb02d67bddfcf33eb5f7b0b
lerasaltykova <134429364+lerasaltykova@users.noreply.github.com>
2024-02-24 15:58:02 -0800

    New hobbies added

--- src/main/java/com/example/studybuddyapi/StudybuddyapiApplication.java
+++ src/main/java/com/example/studybuddyapi/StudybuddyapiApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.studybuddyapi.model.Hobby;
 import com.example.studybuddyapi.model.Institution;
 import com.example.studybuddyapi.model.Program;
 import com.example.studybuddyapi.model.User;
+import com.example.studybuddyapi.repositories.HobbyRepository;
 import com.example.studybuddyapi.repositories.IntitutionRepository;
 import com.example.studybuddyapi.repositories.ProgramRepository;
 import com.example.studybuddyapi.repositories.UserRepository;
@@ -103,6 +105,69 @@ public class StudybuddyapiApplication {
 	}
 	
 	
+	@Bean
+	ApplicationRunner initHobbies (HobbyRepository hobbyRepo) {
+		return args -> {
+			hobbyRepo.save(new Hobby("Basketball","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Golf","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Running","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Soccer","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Volleyball","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Yoga","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Pilates","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Swimming","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Ice skating","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Football","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Stretching","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Hiking","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Cheerleading","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Fishing","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Skiing","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Weight lifting","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Kickboxing","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Boxing","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Board games","Active Hobbies"));
+			hobbyRepo.save(new Hobby("Other","Active Hobbies"));
+
+			hobbyRepo.save(new Hobby("Painting","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Poetry writing","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Jewelry making","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Clay crafts","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Magic","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Pottery/Ceramics","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Makeup art","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Hair styling","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Photography","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Sewing","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Knitting","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Embroidery","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Crochet","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Acting","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Gardening","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Design","Creative Hobbies"));
+			hobbyRepo.save(new Hobby("Other","Creative Hobbies"));
+			
+			hobbyRepo.save(new Hobby("Reading","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Journaling","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Writing","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Crossword puzzles","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Sudoku","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Word scrambles","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Languages","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Games","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Podcasts","Mental Hobbies"));
+			hobbyRepo.save(new Hobby("Other","Mental Hobbies"));
+			
+
+			hobbyRepo.save(new Hobby("Singing","Musical Hobbies"));
+			hobbyRepo.save(new Hobby("Dancing","Musical Hobbies"));
+			hobbyRepo.save(new Hobby("Listening to music","Musical Hobbies"));
+			hobbyRepo.save(new Hobby("Playing instruments","Musical Hobbies"));
+			hobbyRepo.save(new Hobby("Other","Musical Hobbies"));
+			
+			
+		};
+	}
 	
 	@Bean
 	ApplicationRunner initUsers(UserRepository userRepo) {
---
+++ src/main/java/com/example/studybuddyapi/model/Hobby.java
@@ -0,0 +1,59 @@
+package com.example.studybuddyapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "hobby")
+public class Hobby {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "category")
+    private String category;
+
+    
+    
+    public Hobby() {} 
+    
+    
+
+    public Hobby(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}

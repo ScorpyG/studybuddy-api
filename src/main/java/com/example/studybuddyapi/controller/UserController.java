c728f7e8bcafd3e1d4eff86ebe03f1f2593e684d
insanelygreatsoftware <49577084+brutallyhonest523@users.noreply.github.com>
2024-03-05 17:26:09 -0800

    added 	@GetMapping("/users/{id}")

--- src/main/java/com/example/studybuddyapi/controller/UserController.java
+++ src/main/java/com/example/studybuddyapi/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,6 +26,18 @@ public class UserController {
 	
 	@Autowired
 	PairRepository pairRepo;
+	
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+		Optional<User> userData = userRepo.findById(id);
+		
+		if (userData.isPresent()) {
+			return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
 	
 	@PutMapping("/users/{id}")
 	public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
